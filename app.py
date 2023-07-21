18971d469078e427965fbd5ec5ec76fae515975f
Nicolas Mirabet <nmirabets@me.com>
2023-07-21 11:20:35 +0200

    copied project into directory

---
+++ app.py
@@ -0,0 +1,164 @@
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+
+import functions
+
+def update_popularity():
+    
+    pop_dict = {'Hidden Gem': 0,
+                'Underrated': 25,
+                'Trending': 50,
+                'Popular': 75,
+                'Super hit': 100}
+    
+    st.session_state['popularity_min'] = pop_dict[st.session_state['pop_slider'][0]]
+    st.session_state['popularity_max'] = pop_dict[st.session_state['pop_slider'][1]]
+
+def update_model():
+    st.session_state['current_model'] = st.session_state['sm_radio']
+
+def start_again():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+def option_song_clicked():
+    # Load data
+    df = pd.read_csv('./data/songs_and_features_kmeans_clustered.csv', sep=';')
+
+    # Define selected features
+    selected_features = ['energy','danceability','mode', 'speechiness', 
+                         'tempo', 'acousticness', 'instrumentalness', 'valence']
+
+    # Load models
+    kmeans_9 = functions.read_pickle_file('./models/{}.pkl'.format('kmeans_9'))
+    kmeans_27 = functions.read_pickle_file('./models/{}.pkl'.format('kmeans_27'))
+
+    # Store models in a dictionary
+    models = {'kmeans_9': kmeans_9,
+             'kmeans_27': kmeans_27}
+
+    
+    for item in st.session_state.items():
+        if (item[1] == True) & (item[0][:2] == 'so'): # if it is search option button and its true
+            st.session_state['selected_song_index'] = int(item[0][-1])
+            st.session_state['current_page'] = 2
+
+    st.session_state['track_id'] = st.session_state['sp_song_data'][st.session_state['selected_song_index']]['track_id']
+
+    selected_model = st.session_state['current_model']
+
+    st.session_state['recommended_songs'] = functions.recommend_song(df, selected_model, models, selected_features, 
+                                                                     st.session_state['track_id'], 
+                                                                     st.session_state['popularity_min'], 
+                                                                     st.session_state['popularity_max']).to_dict(orient="records")
+
+def main():
+
+    if 'current_page' not in st.session_state:
+        st.session_state['current_page'] = 1
+        
+    if 'current_model' not in st.session_state:
+        st.session_state['current_model'] = 'kmeans_9'
+        
+    if 'popularity_min' not in st.session_state:
+        st.session_state['popularity_min'] = 0
+        st.session_state['popularity_max'] = 100
+
+    #'st.session_state object:', st.session_state
+
+    # Application Title
+    st.title('Song Recommender')
+    st.caption('Beta')
+
+    st.divider()
+    
+    st.select_slider('Select a range of popularity',
+                                          options=['Hidden Gem', 'Underrated', 'Trending', 'Popular', 'Super hit'],
+                                          value=('Hidden Gem', 'Super hit'),
+                                         on_change=update_popularity,
+                                         key='pop_slider')
+    st.divider()
+    
+    # Page rendering
+    if st.session_state['current_page'] == 1:
+
+        # Model Selection
+        st.radio("Select model ", ("kmeans_9","kmeans_27"), horizontal=True, key='sm_radio', on_change=update_model)
+
+        st.divider()
+    
+        # Track name input box
+        track_name = st.text_input('Type a song that you like')
+    
+        # Artist input box
+        artist = st.text_input('Type the artist (optional)')
+        
+        # Search button
+        search_button = st.button('Search song')
+        
+        if search_button:
+            
+            if (track_name == "") & (track_name == ""):
+                # Error message if nothing as input
+                st.error("You must input a song.")
+            else:
+                # Get spotify data including track_id
+                sp_song_data = functions.search_song(track_name, artist)
+                st.session_state['sp_song_data'] = sp_song_data
+                
+                if len(sp_song_data) > 0:
+                    for index, song in enumerate(sp_song_data):
+                        col1, col2 = st.columns(2)
+
+                        with col1:
+                            st.subheader(song['track_name'])
+                            st.caption(song['artist'])
+                            st.write(functions.convert_seconds_to_min_seconds(song['track_duration']))
+                            st.write('Album: *'+str(song['album_name'])+'*')
+                            st.write('Released: ', song['album_release_year'])
+
+                        with col2:
+                            st.image(song['album_image'],width=125)
+                            search_button = st.button('I like this song!', on_click=option_song_clicked, key='so_button_'+str(index))
+
+
+                        st.divider()
+                else:
+                    st.info('No songs found.')
+
+    elif st.session_state['current_page'] == 2:
+        
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader('You will also like...')
+        with col2:
+            st.button('Recommend again', on_click=option_song_clicked)
+        with col3:
+            st.button('Start again', on_click=start_again)
+        st.divider()
+        
+        recommended_songs = st.session_state['recommended_songs']
+        if len(recommended_songs) == 0:
+            st.info('No recommendations available. Consider widening the popularity filter.')
+        for index, song in enumerate(recommended_songs):
+            col1, col2 = st.columns(2)
+    
+            with col1:
+                st.subheader(song['track_name'])
+                st.caption(song['artist'])
+                st.write(functions.convert_seconds_to_min_seconds(song['track_duration']))
+                st.write('Album: *'+str(song['album_name'])+'*')
+                st.write('Released: ', song['album_release_year'])
+                st.write('Popularity: ', song['popularity'])
+    
+            with col2:
+                st.image(song['album_image'], width=125)
+                #st.image('./resources/images/listen-on-spotify-3.png', width=125)
+                st.markdown('[Listen on Spotify!](' + song['track_link'] + ')')
+
+            st.divider()
+                    
+if __name__ == "__main__":
+    main()
---
+++ functions.py
@@ -0,0 +1,236 @@
+import pandas as pd
+import pickle
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from IPython.display import clear_output
+
+from credentials import *
+
+def read_pickle_file(path: str):
+    '''
+
+    This function reads pickle files such as scalers, transformers, encoders or models 
+    from a previsouly saved file using save_pickle_file.
+    
+    Input:
+        path: the file's location with .pkl extension. i.e encoders/encoder.pkl
+    
+    Output:
+        file: the file read at the directory
+    
+    '''
+    
+    with open(path, "rb") as file:
+        file_read = pickle.load(file)
+
+    return file_read
+
+
+def search_song(title: str, artist: str) -> list:
+    '''
+    Given a song title and artist, this function returns up to 5 posible matches
+    from Spotipy, Spotify's API.
+    
+    Input:
+    title: song title as string
+    artist: song artist as string
+    
+    Output:
+    List of matches, each match is a dictionary
+    
+    '''
+    
+    # Initialize SpotiPy with user credentias
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=Client_ID,
+                                                           client_secret=Client_Secret))
+
+    # Create the query string
+    query = ("track:"+title if title != "" else "")+" "+("artist:"+artist if artist != "" else "")
+
+    # Search the requested song and artist if the input is not blank
+    if len(query) > 0:
+        try:
+            raw_results = sp.search(q=query,type="track,artist",limit=5)
+            raw_results = raw_results['tracks']['items'] # Parse first two dictionary levels
+        except Exception as e:
+            print("Error:", e)
+    else:
+        raw_results = []
+    
+    # Define result variable
+    clean_results = []
+    
+    # Loop through results and store relevant data in clean_results as list of dictionaries
+    for index, result in enumerate(raw_results):
+        
+        # Store each individual result in temporal dictionary             TO-DO: add -> song poopularity, artist nationality...
+        clean_result = {'result_index': index,
+                        'track_id': result['id'],
+                        'href': result['href'],
+                        'track_link': result['external_urls']['spotify'],
+                        'track_name': result['name'],
+                        'artist': result['album']['artists'][0]['name'],
+                        'album_name': result['album']['name'],
+                        'album_release_year': result['album']['release_date'],
+                        'track_duration': round(result['duration_ms']/1000),
+                        'album_image': result['album']['images'][1]['url'],
+        }
+        
+        # Append each dictionary to results list
+        clean_results.append(clean_result)
+    
+    return clean_results
+
+
+def get_audio_features(track_ids: list) -> pd.DataFrame:
+    '''
+    Given a list of track IDs, this function returns 
+    track audio features such as danceability, energy or tempo
+    using Spotipy, Spotify's API.
+    
+    Input:
+    track_ids: list of Spotipy track_ids
+    
+    Output:
+    DataFrame with the track_ids and audio features
+    
+    '''
+    
+    # Define loop pauses to avoid API server blockage by overflow
+    sleep_time = 30 # seconds of sleep time
+    loop_count = 50 # tracks per sleep loop
+    
+    # Initialize SpotiPy with user credentias
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=Client_ID,
+                                                           client_secret=Client_Secret))
+
+    # Define result variable
+    clean_results = []
+    
+    loop_counter = 0 # counter to track loop
+    counter = 0 # counter to track progress
+    fail_counter = 0 # counter to track failed matches
+    
+    # Loop through track_ids to get relevant audio features
+    for index, track_id in enumerate(track_ids):
+        
+        counter += 1 # Add 1 to global counter
+        clear_output(wait=True) # Clear print output to update progress
+        print('Downloading audio features...', round(((index + 1) / len(track_ids))*100), '%') # Print progress
+        
+        #try:
+            # Get audio features from Spotipy
+        api_result = sp.audio_features(track_id)[0]
+        #print(sp.audio_features(track_id))
+
+        # Add features to track variable
+        track = {'track_id': track_id,
+                 'acousticness': api_result['acousticness'],
+                 'danceability': api_result['danceability'],
+                 'energy': api_result['energy'],
+                 'instrumentalness': api_result['instrumentalness'],
+                 'key': api_result['key'],
+                 'liveness': api_result['liveness'],
+                 'loudness': api_result['loudness'],
+                 'mode': api_result['mode'],
+                 'speechiness': api_result['speechiness'],
+                 'tempo': api_result['tempo'],
+                 'time_signature': api_result['time_signature'],
+                 'valence': api_result['valence'],
+        }
+
+        # Append track results to results list
+        clean_results.append(track)
+
+        #except Exception as e:
+            # If there is an error:
+        #    print("Error:", e)
+        #    fail_counter += 1
+        
+        # Sleep to avoid API overflow...
+        loop_counter += 1
+        if loop_counter >= loop_count:
+            loop_counter = 0
+            # Generate a random sleep timer based on sleep_time
+            random_sleep_timer = random.randint(int(sleep_time*0.5), int(sleep_time*1.5))
+            print('Sleeping for', random_sleep_timer, 's...')
+            time.sleep(random_sleep_timer) # Sleep
+            
+    
+    clear_output(wait=True)
+    print("Audio features download complete.", "Success rate:", round(((counter-fail_counter)/counter)*100), "%")
+    print("Succesful downloads:",counter-fail_counter,"Failed downloads:",fail_counter)
+    
+    # Convert list of dictionaries to DataFrame
+    clean_results_df = pd.DataFrame(clean_results)
+
+    return clean_results_df
+
+
+def is_song_hot(df: pd.DataFrame, track_id: str) -> bool:
+    '''
+    '''
+    
+    if len(df[(df['track_id'] == track_id)]['is_hot']) == 0:
+        return False 
+    else:
+        return df[(df['track_id'] == track_id)]['is_hot'][:1].item()
+
+
+def scale_user_song(X: pd.DataFrame) -> pd.DataFrame:
+    '''
+    '''
+    scaler = read_pickle_file('./scalers/scaler.pkl')
+    
+    # Transform dataset
+    X_scaled = scaler.transform(X)
+
+    # Convert numpy array into pandas dataframe
+    X_scaled_df = pd.DataFrame(X_scaled, columns = X.columns)
+    
+    return X_scaled_df
+
+
+def convert_seconds_to_min_seconds(seconds):
+    '''
+    '''
+    
+    minutes = seconds // 60
+    seconds %= 60
+    
+    return f"{minutes:02d}:{seconds:02d}"
+
+def recommend_song(df: pd.DataFrame, selected_model: str, models, selected_features: list, track_id: str, min_popularity: int, max_popularity: int) -> list:
+    '''
+    '''
+    
+    # Get if user song is hot
+    is_hot = is_song_hot(df, track_id)
+    
+    # Get the audio features from Spotify for the song selected by user
+    sp_song_audio_features = get_audio_features([track_id])[selected_features]
+    
+    # Work around while max retries ban
+    #sp_song_audio_features = df[df['track_id'] == track_id][selected_features]
+    
+    # Scale user selected song
+    sp_song_audio_features_scaled = scale_user_song(sp_song_audio_features)
+    
+    # Predict cluster of user song
+    recommended_cluster = models[selected_model].predict(sp_song_audio_features_scaled)[:1].item()
+    
+    # Get a song from database using recommended cluster + is hot / not hot
+    recommended_songs = df[(df['clusters_{}'.format(selected_model)] == recommended_cluster) & 
+                          (df['is_hot'] == is_hot) & 
+                          (df['popularity'] >= min_popularity) &
+                          (df['popularity'] <= max_popularity)
+                         ]
+    min_results = 5
+    # Check if df has a minimum of 5 songs
+    if recommended_songs.shape[0] < min_results:
+        recommended_songs = recommended_songs.sample(n=recommended_songs.shape[0])
+    else:
+        recommended_songs = recommended_songs.sample(n=min_results)
+    
+    return recommended_songs
+    
